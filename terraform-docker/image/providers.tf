188824bcb1b04d35c030379cdd44f64029d9c43b
Ubuntu <ubuntu@ip-172-31-85-223.ec2.internal>
2022-06-30 01:42:02 +0000

    updated for week21 coursework

--- terraform-docker/image/providers.tf
+++ terraform-docker/image/providers.tf
@@ -1,10 +1,8 @@
 terraform {
   required_providers {
     docker = {
-      source = "kreuzwerker/docker"
+      source  = "kreuzwerker/docker"
+      version = "~> 2.8.0"
     }
   }
 }
-
-
-provider "docker" {}
--- terraform-docker/outputs.tf
+++ terraform-docker/outputs.tf
@@ -1,9 +1,4 @@
-output "container-name" {
-  value       = docker_container.nodered_container[*].name
-  description = "The name of the container"
-}
-
-output "ip-address" {
-  value       = [for i in docker_container.nodered_container[*] : join(":", [i.ip_address], i.ports[*]["external"])]
-  description = "The IP address and external port of the container"
+output "application_access" {
+  value       = [for x in module.container[*] : x]
+  description = "The name and socket for each application."
 }
